0525107885c1b1e6657066f7124c5a25c1fd66bf
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-05-15 00:26:43 +0200

    update, delete, get one user, create user implemented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,4 +12,26 @@ router.get('/', (req, res) => {
     res.send(users);
 });
 
+router.get('/:id', (req, res) => {
+    const _id = { id: +req.params.id };
+    const user = UserService.search(_id);
+    res.send(user);
+});
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    res.send(UserService.create(user));
+});
+
+router.put('/:id', (req, res) => {
+    const _id = +req.params.id;
+    const dataToUpdate = req.body;
+    res.send(UserService.update(_id, dataToUpdate));
+});
+
+router.delete('/:id', (req, res) => {
+    const _id = +req.params.id;
+    res.send(UserService.delete(_id));
+});
+
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -18,7 +18,19 @@ class UserService {
         }
         return users;
     }
-
+    create(data) {
+        const user = UserRepository.create(data);
+        if(!user) {
+            return null;
+        }
+        return user;
+    }
+    update(id, data) {
+        return UserRepository.update(id, data);
+    }
+    delete(id) {
+        return UserRepository.delete(id);
+    }
 
 }
 
